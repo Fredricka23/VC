01edf5841588dd901b1aafcb3f932441c22ef9a3
Fredricka David <110636706+Fredricka23@users.noreply.github.com>
2022-11-06 22:49:06 +0200

    Add files via upload

---
+++ add.py
@@ -0,0 +1,11 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> {
+... "DG": "Test",
+... "Data Glacier": "Golf"
+... }
+... "Data Glacier": "Golf",
+... "Fredricka Data": "Golf",
+... "Fredricka David": "Golf",
+... }
+SyntaxError: multiple statements found while compiling a single statement
